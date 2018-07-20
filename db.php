dccc22758275a2bd2d760e90147317a5859a4d2d
naatikah <38623734+naatikah@users.noreply.github.com>
2018-07-20 23:12:59 +0800

    Add files via upload

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "fyp";
+
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+
+?> 
---
+++ try.php
@@ -0,0 +1,24 @@
+<?php
+	include_once "db.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+		$sql = "SELECT * FROM user;";
+		$result = mysqli_query($conn, $sql);
+		$resultCheck = mysqli_num_rows($result);
+
+		if($resultCheck > 0){
+			while ($row = mysqli_fetch_assoc($result)){
+				echo $row['Name'] . "<br>";
+			}
+		}
+?>
+
+</body>
+</html>
