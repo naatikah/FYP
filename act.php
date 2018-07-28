394ac2cb5ceb9003170032bce97ed89e938da45a
GitTimeOut <32845258+GitTimeOut@users.noreply.github.com>
2018-07-28 14:31:38 +0800

    Add files via upload

--- act.php
+++ act.php
@@ -2,8 +2,15 @@
 session_start();
 include "dbh.php"; 
 $id = $_GET['id'];
+$pid = $_GET['pid'];
+$paydate=date("Y-m-d");
+$paytime=date("H:i:s");
 $query= "UPDATE offer SET StatusID = 11 WHERE OfferID = '$id'";
 $result=mysqli_query($link,$query) or die(mysqli_error($link));
+
+$query1= "UPDATE payment SET PayTime='$paytime', PayDate='$paydate' WHERE PaymentID = '$pid'";
+$result=mysqli_query($link,$query1) or die(mysqli_error($link));
+
 ?>
 
 <!DOCTYPE html>
