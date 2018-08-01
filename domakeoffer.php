206c147f185d735821456cba070587917c4dceb9
syakirmik <39413014+syakirmik@users.noreply.github.com>
2018-08-01 14:19:50 +0800

    Add files via upload

--- domakeoffer.php
+++ domakeoffer.php
@@ -4,6 +4,7 @@
 	$bookingid = $_GET['id'];
 	$userid = $_SESSION['userid']
 	$comments = $_POST['comments'];
+	$message = "";
 
     // build sql insert query
 	$sql = "INSERT INTO offer(OfferID, StatusID, UserID, BookingID, DistrictID, Comments) VALUES (NULL, 4, $userid, $bookingid, 1, '$comments')";
@@ -26,7 +27,7 @@
 </head>
 <body>
 	<?php
-
+	     echo $message
 	?>
 </body>
 </html>
